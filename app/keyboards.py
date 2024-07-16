4bd3f08209f415c702ca5b74bd08ed27a3841aa6
NachtlicherKater <124313831+NachtlicherKater@users.noreply.github.com>
2024-07-16 04:14:46 +0300

    Delete app directory

--- app/keyboards.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='11')],
-    [KeyboardButton(text='22'), KeyboardButton(text='22')]
-])
